276dd29bc52015bd7b08f3a2cd96bccd14f20d0c
pancham99 <pancham.047@gmail.com>
2023-02-10 15:31:33 +0530

    changed done

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Photo from '../components/Photo'
 const Home = () => {
   return (
-    <div className='container mx-auto py-16'>
+    <div className='container mx-auto py-12 '>
       {/* grid */}
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4 max-w-sm md:max-w-none md:mx-0'>
         <Photo/>
